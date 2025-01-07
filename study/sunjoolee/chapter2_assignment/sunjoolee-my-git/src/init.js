aa1d8c3f787fde883c6c37302382601fd9c95f2c
sunjoolee <spottypoo124@gmail.com>
2025-01-07 22:13:54 +0900

    [sunjoolee/1주차]: Git 구현 - 명령 실행 전, 로컬 레포인지 검사

--- study/sunjoolee/chapter2_assignment/sunjoolee-my-git/src/init.js
+++ study/sunjoolee/chapter2_assignment/sunjoolee-my-git/src/init.js
@@ -14,32 +14,24 @@ const { glob } = pkg;
 /**
  * - 현재 디렉토리에 .mygit, .mygitignore 파일 생성
  * - 현재 디렉토리에 .myStagingArea 디렉토리 생성
- * - 이미 저장소가 생성된 디렉토리인 경우, throw error
  * @function
  * @example my-git init
  */
 export default function myGitInit() {
-
   const curDir = process.cwd()
+
+  // .my-git 파일 생성
   const myGitPath = path.join(curDir, MY_GIT_FILE)
-  fs.access(myGitPath, fs.constants.F_OK, error => {
-    // .mygit 파일에 접근할 수 있는 경우, 이미 저장소 생성되어있음
-    if (!error) {
-      console.error("my-git is already initiated")
-      process.exit()
-    }
+  writeToDst(myGitPath, "# This is .mygit file.")
 
-    // .my-git 파일 생성
-    writeToDst(myGitPath, "# This is .mygit file.")
+  // .my-gitignore 파일 생성
+  const myGitIgnorePath = path.join(curDir, MY_GIT_IGNORE_FILE)
+  writeToDst(myGitIgnorePath, "# This is .mygitignore file.")
 
-    // .my-gitignore 파일 생성
-    const myGitIgnorePath = path.join(curDir, MY_GIT_IGNORE_FILE)
-    writeToDst(myGitIgnorePath, "# This is .mygitignore file.")
-
-    // stage 디렉토리 생성
-    const stagePath = path.join(curDir, MY_STAGING_AREA)
-    fs.ensureDir(stagePath, (error) => {
-      if (error) console.error(`Cannot ensure path ${stagePath}\n`, error)
-    })
+  // stage 디렉토리 생성
+  const stagePath = path.join(curDir, MY_STAGING_AREA)
+  fs.ensureDir(stagePath, (error) => {
+    if (error) console.error(`Cannot ensure path ${stagePath}\n`, error)
   })
 }
+
--- study/sunjoolee/chapter2_assignment/sunjoolee-my-git/src/my-git.js
+++ study/sunjoolee/chapter2_assignment/sunjoolee-my-git/src/my-git.js
@@ -1,6 +1,7 @@
 import pkg from 'glob';
 import myGitAdd from './add.js';
 import myGitInit from './init.js';
+import isLocalRepo from './is-local-repo.js';
 const { glob } = pkg;
 
 /**
@@ -9,17 +10,24 @@ const { glob } = pkg;
  * @param {string[] | undefined} args my-git 명령어 인수 목록
  */
 export default function myGit(command, args) {
-  switch (command) {
-    case 'init':
-      myGitInit()
-      break
-    case 'add':
-      myGitAdd(args)
-      break
-    default:
-      console.error("Unknown my-git function!", command)
-      process.exit()
-  }
+  isLocalRepo((isLocalRepo) => {
+    if (command === 'init') {
+      // init 명령어인 경우, 로컬 저장소가 아니어야
+      if (isLocalRepo) console.error("my-git is already initiated")
+      else myGitInit()
+    } else {
+      // init 외 명령어인 경우, 로컬 저장소이어야
+      if (!isLocalRepo) console.error("my-git is not initiated")
+      else switch (command) {
+        case 'add':
+          myGitAdd(args)
+          break
+        default:
+          console.error("Unknown my-git function!", command)
+          process.exit()
+      }
+    }
+  })
 }
 
 
